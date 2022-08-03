7f708a9e2f26dd33b067a0b4cce74a13ccc2d7da
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2022-08-04 04:35:52 +0800

    Update HydaelynAttack_setup.sh

--- HydaelynAttack_setup.sh
+++ HydaelynAttack_setup.sh
@@ -9,7 +9,7 @@ pip install tqdm > /dev/null 2>&1
 pip install pyfiglet==0.7 > /dev/null 2>&1
 clear
 echo -e "\033[92m[*]\033[0m \033[1;34mHydaelynAttack was installed successfully.\033[0m'\n\n"
-echo -e "\n\nErrors? If it's about libraries, just write these strings:[0m'\n"
-echo -e "\n  pip install tqdm[0m'"
-echo -e "\n  pip install pyfiglet[0m'\n"
+echo -e "\n\n Errors? If it's about libraries, just write these strings:'\n"
+echo -e "\n   pip install tqdm'"
+echo -e "\n   pip install pyfiglet[0m'"
 echo -e "\n\nTo run it type '\033[1;4mpython3 HydaelynAttack.py\033[0m'\n\n"
