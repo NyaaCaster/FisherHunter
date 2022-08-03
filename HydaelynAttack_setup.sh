5ad2679fc853053a68f29bd2db051cde203e7a7b
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2022-08-04 04:41:47 +0800

    Update HydaelynAttack_setup.sh

--- HydaelynAttack_setup.sh
+++ HydaelynAttack_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "                Installing \033[1;34mHydaelynAttack\033[0m'\n\n"
+echo -e "                Installing \033[1;34mHydaelynAttack\033[0m\n\n"
 echo -e "                Please Wait..."
 apt-get -y install python-pip > /dev/null 2>&1
 apt-get -y install python3-pip > /dev/null 2>&1
@@ -8,8 +8,8 @@ python3 -m pip install --upgrade pip > /dev/null 2>&1
 pip install tqdm > /dev/null 2>&1
 pip install pyfiglet==0.7 > /dev/null 2>&1
 clear
-echo -e "\033[92m[*]\033[0m \033[1;34mHydaelynAttack was installed successfully.\033[0m'\n\n"
-echo -e "\n\n Errors? If it's about libraries, just write these strings:\n"
+echo -e "\033[92m[*]\033[0m \033[1;34mHydaelynAttack\033[0m was installed successfully.\n\n"
+echo -e "\n\n Errors? If it's about libraries, just write these strings:"
 echo -e "\n   pip install tqdm"
 echo -e "\n   pip install pyfiglet"
 echo -e "\n\n To run it type '\033[1;4mpython3 HydaelynAttack.py\033[0m'\n\n"
