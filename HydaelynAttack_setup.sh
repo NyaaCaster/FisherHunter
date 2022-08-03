398080671c1254991670f9597554420e2a821dda
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2022-08-04 04:36:14 +0800

    Update HydaelynAttack_setup.sh

--- HydaelynAttack_setup.sh
+++ HydaelynAttack_setup.sh
@@ -12,4 +12,4 @@ echo -e "\033[92m[*]\033[0m \033[1;34mHydaelynAttack was installed successfully.
 echo -e "\n\n Errors? If it's about libraries, just write these strings:'\n"
 echo -e "\n   pip install tqdm'"
 echo -e "\n   pip install pyfiglet[0m'"
-echo -e "\n\nTo run it type '\033[1;4mpython3 HydaelynAttack.py\033[0m'\n\n"
+echo -e "\n\n To run it type '\033[1;4mpython3 HydaelynAttack.py\033[0m'\n\n"
